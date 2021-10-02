31192eff3ab78115b47caa8b2e3b645add0b578e
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-02 14:53:49 +0530

    take object of sensor manager.

--- app/src/main/java/com/example/playmusicusinggravity/MainActivity.java
+++ app/src/main/java/com/example/playmusicusinggravity/MainActivity.java
@@ -2,15 +2,19 @@ package com.example.playmusicusinggravity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     MediaPlayer mp;
+    SensorManager sm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mp = MediaPlayer.create(this, R.raw.y);
+
     }
 }
