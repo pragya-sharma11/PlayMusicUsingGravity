9efa623df3766cb5e32baa9a28c2068a67e2ee77
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-02 14:52:20 +0530

    add music system in code.

--- app/src/main/java/com/example/playmusicusinggravity/MainActivity.java
+++ app/src/main/java/com/example/playmusicusinggravity/MainActivity.java
@@ -2,13 +2,15 @@ package com.example.playmusicusinggravity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    MediaPlayer mp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mp = MediaPlayer.create(this, R.raw.y);
     }
 }
