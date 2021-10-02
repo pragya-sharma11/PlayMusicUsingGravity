749a2c71f5118b5776fa36c39f60d4af55c374e9
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-02 14:59:55 +0530

    get textview by id.

--- app/src/main/java/com/example/playmusicusinggravity/MainActivity.java
+++ app/src/main/java/com/example/playmusicusinggravity/MainActivity.java
@@ -8,15 +8,18 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     MediaPlayer mp;
     SensorManager sm;
     Sensor s;
+    TextView t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        t = findViewById(R.id.textView);
         mp = MediaPlayer.create(this, R.raw.y);
         sm = (SensorManager)getSystemService(SENSOR_SERVICE);
         s = sm.getDefaultSensor(Sensor.TYPE_GRAVITY);
@@ -27,6 +30,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void onSensorChanged(SensorEvent event) {
         if(event.values[0]>1){
             mp.start();
+
         }
     }
 
